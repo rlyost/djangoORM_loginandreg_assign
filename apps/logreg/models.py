49d5fe0f5ffcba23a940bb36105ec78a8cd0fb00
rlyost <ry@ryost.us>
2018-03-23 16:19:10 -0700

    reworked validation into model

--- apps/logreg/models.py
+++ apps/logreg/models.py
@@ -1,7 +1,61 @@
 from __future__ import unicode_literals
-
 from django.db import models
+import bcrypt
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
+class UserManager(models.Manager):
+    def log_validator(self, postData):
+        errors = {}
+        log_email = postData['logemail']
+        log_check = User.objects.filter(email=log_email)
+        # Validates email address for proper format.
+        if len(log_email) < 1:
+            errors["email"] = "Email cannot be blank!"
+            return errors
+        elif not EMAIL_REGEX.match(log_email):
+            errors["email"] = "Invalid Email Address!"
+            return errors
+        elif len(log_check) == 0:
+            errors["email"] = "Email not found!"
+            return errors
+        log_pass = User.objects.get(email=log_email).password
+        #password validation - compares hashed logged in password to hashed Db password
+        if not bcrypt.checkpw(postData['logpassword'].encode(), log_pass.encode()):
+            errors["email"] = "Password does not match."
+            return errors
+    def reg_validator(self, postData):
+        errors = {}
+        #grabs record if email exists in the Db
+        reg_check = User.objects.filter(email=postData['regemail'])
+        # Checks name fields
+        if len(postData['fname']) < 2 or len(postData['lname']) < 2:
+            errors["name"] = "Name must be longer."
+            return errors
+        elif not str(postData['fname']).isalpha():
+            errors["name"] = "First Name can only be letters."
+            return errors       
+        elif not str(postData['lname']).isalpha():
+            errors["name"] = "Last Name can only be letters."
+            return errors
+        # Validates email address for proper format.
+        if len(postData['regemail']) < 1:
+            errors["email"] = "Email cannot be blank!"
+            return errors
+        elif not EMAIL_REGEX.match(postData['regemail']):
+            errors["email"] = "Invalid Email Address!"
+            return errors
+        elif len(reg_check) != 0:
+            errors["email"] = "Duplicate address, please enter another one or try logging in!"
+            return errors
+        # Check password length and matching confirmation
+        if len(postData['regpassword']) < 8:
+            errors["password"] = "Password is not long enough!"
+            return errors
+        elif postData['regpassword'] != postData['regpassword2']:
+            errors["password"] = "Passwords do not match!"
+            return errors
+    
 class User(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255, default='')
@@ -9,3 +63,5 @@ class User(models.Model):
     password = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
+
+    objects = UserManager()
--- apps/logreg/views.py
+++ apps/logreg/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.core.urlresolvers import reverse
 from time import gmtime, strftime
-from .models import User    
+from .models import *    
 import bcrypt
 import re
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
@@ -16,92 +16,42 @@ def index(request):
 
 def registration(request):
     if request.method == "POST":
-        #grabs record if email exists in the Db
-        reg_check = User.objects.filter(email=request.POST['regemail'])
         #hashes password for storage to the Db        
         hashed_password = bcrypt.hashpw(request.POST['regpassword'].encode(), bcrypt.gensalt(13))
-
         request.session['fname'] = request.POST['fname']
-        lname = request.POST['lname']
-        email = request.POST['regemail']
-        password = request.POST['regpassword']
-        password2 = request.POST['regpassword2']
-    
-        # Checks name fields
-        if len(request.session['fname']) < 2 or len(lname) < 2:
-            messages.warning(request, "Name must be longer.")
+        errors = User.objects.reg_validator(request.POST)
+        if errors != None:
+            for tag, error in errors.iteritems():
+                messages.error(request, error, extra_tags=tag)
             return redirect('/')
-        elif not str(request.session['fname']).isalpha():
-            messages.warning(request, "First Name can only be letters.")
-            return redirect('/')       
-        elif not str(lname).isalpha():
-            messages.warning(request, "Last Name can only be letters.")
-            return redirect('/')
-      
-        # Validates email address for proper format.
-        if len(email) < 1:
-            messages.warning(request, "Email cannot be blank!")
-            return redirect('/')
-        elif not EMAIL_REGEX.match(email):
-            messages.warning(request, "Invalid Email Address!")
-            return redirect('/')
-        elif len(reg_check) != 0:
-            messages.warning(request, "Duplicate address, please enter another one or try logging in!")
-            return redirect('/')
-        
-        # Check password length and matching confirmation
-        if len(password) < 8:
-            messages.warning(request, "Password is not long enough!")
-            return redirect('/')
-        elif password != password2:
-            messages.warning(request, "Passwords do not match!")
-            return redirect('/')
-        password = ''
-        password2 = ''
-
-        request.session['from'] = 0
-        #creates a new validated record in database for new user
-        User.objects.create(first_name=request.session['fname'], last_name=lname, email=email, password=hashed_password)
-        #sets logged in user for use later
-        request.session['id'] = User.objects.last().id
-        request.session['from'] = 0
-        #redirects to success to display sucessful login
-    return redirect('usuccess')
+        else:
+            request.session['from'] = 0
+            #creates a new validated record in database for new user
+            User.objects.create(first_name=request.session['fname'], last_name=request.POST['lname'], email=request.POST['regemail'], password=hashed_password)
+            #sets logged in user for use later
+            request.session['id'] = User.objects.last().id
+            request.session['from'] = 0
+            #redirects to success to display sucessful login
+            return redirect('usuccess')
 
 # LOGIN *************************************************
 
 def login(request):
     if request.method == "POST":
-        #Checks Db for log email address. If not there returns empty list
-        log_email = request.POST['logemail']
-        log_check = User.objects.filter(email=log_email)
-        
-        # Validates email address for proper format.
-        if len(log_email) < 1:
-            messages.warning(request, "Email cannot be blank!")
+        errors = User.objects.log_validator(request.POST)
+        if errors != None:
+            for tag, error in errors.iteritems():
+                messages.error(request, error, extra_tags=tag)
             return redirect('/')
-        elif not EMAIL_REGEX.match(log_email):
-            messages.warning(request, "Invalid Email Address!")
-            return redirect('/')
-        elif len(log_check) == 0:
-            messages.warning(request, "Email not found!")
-            return redirect('/')
-
-        log_pass = User.objects.get(email=log_email).password
-
-        #password validation - compares hashed logged in password to hashed Db password
-        if not bcrypt.checkpw(request.POST['logpassword'].encode(), log_pass.encode()):
-            messages.warning(request, "Password does not match.")
-            return redirect('/')
-
-        #sets logged in user id for use later  
-        request.session['id'] = User.objects.get(email=log_email).id
-        #sets First name for user interaction
-        request.session['fname'] = User.objects.get(email=log_email).first_name
-        #sets login or registration indicator
-        request.session['from'] = 1
-        #redirects to success to display sucessful login
-    return redirect('usuccess')
+        else:
+            #sets logged in user id for use later  
+            request.session['id'] = User.objects.get(email=request.POST['logemail']).id
+            #sets First name for user interaction
+            request.session['fname'] = User.objects.get(email=request.POST['logemail']).first_name
+            #sets login or registration indicator
+            request.session['from'] = 1
+            #redirects to success to display sucessful login
+            return redirect('usuccess')
 
 # SUCCESSFUL LOGIN OR REGISTRATION *************************************************
 
